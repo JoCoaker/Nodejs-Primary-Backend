c97e2304034f4944eb820bb80b230305faabc985
JoCoaker <tuckery7@gmail.com>
2018-05-08 10:10:40 +0200

    Update base.router.js

--- src/app/core-module/base/base.router.js
+++ src/app/core-module/base/base.router.js
@@ -9,19 +9,11 @@ const Router = require('express').Router;
 class BaseRouter {
 
     constructor(prefix) {
-        this._prefix = prefix;
-        this._mRouter = Router();
+        this.mPrefix = prefix;
+        this.mRouter = Router();
     }
 
-    getRouter() {
-        return this._mRouter;
-    }
-
-    getPrefix() {
-        return this._prefix;
-    }
-
-    _checkRequired(required, req) {
+    checkRequired(required, req) {
         for (let i = 0; i < required.length; i++) {
             if (!req.body[required[i]])
                 return false;
